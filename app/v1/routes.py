107e0c9e3db7cf61aa86b5d8091d536d197367a2
bedann <bedank6@gmail.com>
2019-02-04 22:00:50 +0300

    [Starts #163682932] added necessary imports

--- app/v1/routes.py
+++ app/v1/routes.py
@@ -1,3 +1,11 @@
 from flask import Blueprint
+from flask import request
+from flask import jsonify
 
 bp = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
+@bp.route('/parties', methods=('POST'))
+def create_party():
+    if request.method == 'POST':
+        name = request.form
