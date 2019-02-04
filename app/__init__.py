c5bd4b9fc47db3ce6646ae6bffcc08288ccc390b
bedann <bedank6@gmail.com>
2019-02-04 21:48:58 +0300

    [Chore #163705315] update routes.py with api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask
 from instance.config import app_config
+from .v1 import routes
 
 
 def create_app(config_name):
@@ -10,5 +11,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    # register blueprints
+    app.register_blueprint(routes.bp)
 
     return app
