fedec7b20ca92faebf86bdd8d3ae3642bfce02a1
bedann <bedank6@gmail.com>
2019-02-04 23:25:02 +0300

    [Finishes #163682932] code optimization

--- app/v1/routes.py
+++ app/v1/routes.py
@@ -10,37 +10,31 @@ party_list = []
 
 @bp.route('/parties', methods=['POST'])
 def create_party():
-    if request.method == 'POST':
+    try:
         data = request.get_json()
         name = data['name']
         hq_address = data['hq_address']
         logo_url = data['logo_url']
         slogan = data['slogan']
+    except KeyError as e:
+        return response(
+            "Failed", "{} field is required".format(e.args[0]), 400)
 
-        party = {
-            "id": generate_id(party_list),
-            "name": name,
-            "hq_address": hq_address,
-            "logo_url": logo_url,
-            "slogan": slogan
-        }
+    party = {
+        "id": generate_id(party_list),
+        "name": name,
+        "hq_address": hq_address,
+        "logo_url": logo_url,
+        "slogan": slogan
+    }
 
-        validate_party(party)
+    validate_party(party)
 
-        # append new party to list
-        party_list.append(party)
+    # append new party to list
+    party_list.append(party)
 
-        # return list of parties to display added party
-        return make_response(jsonify({
-            "status": "OK",
-            "message": "Party created successfully",
-            "data": party
-        }), 201)
-    else:
-        return make_response(jsonify({
-            "status": "Failed",
-            "Message": "{} method not allow here".format(request.method)
-        }), 405)
+    # return list of parties to display added party
+    return response("OK", "Party created successfully", 201, party)
 
 
 def validate_party(party):
@@ -57,3 +51,14 @@ def generate_id(list):
     """ Creates a unique ID for a new item to be added to the list"""
 
     return len(list) + 1
+
+
+def response(status, message, code, data=None):
+    """ Creates a basic reposnse """
+    response = {
+        "status": status,
+        "message": message,
+    }
+    if data:
+        response["data"] = data
+    return make_response(jsonify(response), code)
