e5502607ce4e3fb7891584c99a085d8cb1f7793a
bedann <bedank6@gmail.com>
2019-02-14 02:08:19 +0300

    [Finishes #163897181] add admin rights

--- app/v2/views/parties.py
+++ app/v2/views/parties.py
@@ -3,14 +3,21 @@ from flask import request
 from flask import jsonify
 from flask import make_response
 from app.v2.models.party_model import Party
-from app.v2.utils.validator import response, exists, response_error
+from app.v2.models.user_model import User
+from app.v2.utils.validator import response, exists, response_error, not_admin
 from app.blueprints import v2 as bp
+from flask_jwt_extended import (jwt_required)
 
 
 @bp.route('/parties', methods=['POST', 'GET'])
+@jwt_required
 def create_party():
     if request.method == 'POST':
         """ Create party end point """
+
+        restricted = not_admin()
+        if restricted:
+            return restricted
 
         data = request.get_json()
 
@@ -23,7 +30,8 @@ def create_party():
             logo_url = data['logo_url']
             slogan = data['slogan']
         except KeyError as e:
-            return response_error("{} field is required".format(e.args[0]), 400)
+            return response_error(
+                "{} field is required".format(e.args[0]), 400)
 
         party = Party(name, hq_address, logo_url, slogan)
 
@@ -43,6 +51,7 @@ def create_party():
 
 
 @bp.route('/parties/<int:id>', methods=['GET', 'DELETE'])
+@jwt_required
 def get_party(id):
 
     model = Party()
@@ -54,13 +63,21 @@ def get_party(id):
     if request.method == 'GET':
         return response('Success', 200, [data])
     else:
+        restricted = not_admin()
+        if restricted:
+            return restricted
         party = model.from_json(data)
         party.delete(party.id)
         return response('Success', 200, [data])
 
 
 @bp.route('/parties/<int:id>/<string:name>', methods=['PATCH'])
+@jwt_required
 def edit_party(id, name):
+
+    restricted = not_admin()
+    if restricted:
+        return restricted
 
     model = Party()
     data = model.find_by('id', id)
