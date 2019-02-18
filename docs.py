a899921d0931379e5766fbff430b334142b40d6f
bedann <bedank6@gmail.com>
2019-02-18 20:47:11 +0300

    [Starts #164052078] added docs yaml

--- docs.py
+++ docs.py
@@ -3,14 +3,27 @@ from flasgger import Swagger
 
 
 app = create_app('production')
-swagger = Swagger(app, template_file='../apidocs.yaml')
+swagger = Swagger(app, template_file='../apidocs2.yaml')
 
 
-@app.route('/parties')
-def colors(palette):
-    """
-    file:apidocs.yaml
-    """
+# @app.route('/api/v2/auth/signup', methods=['POST'])
+# def signup():
+#     """ endpoint for registering users.
+#     ---
+#     parameters:
+#       - name: username
+#         required: true
+#         type: string
+#       - name: email
+#         type: string
+#         required: true
+#       - name: password
+#         type: string
+#         required: true
+#       - name: lastname
+#         type: string
+#         required: true
+#     """
 
 
 app.run(debug=True)
