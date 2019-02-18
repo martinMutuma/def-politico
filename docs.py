076e79c696a9db3b9e582b9d6f4de97965caf631
bedann <bedank6@gmail.com>
2019-02-18 19:05:37 +0300

    [Chore #164052078] add apidocs.yaml

--- docs.py
+++ docs.py
@@ -1,4 +1,16 @@
 from app import create_app
+from flasgger import Swagger
 
 
 app = create_app('production')
+swagger = Swagger(app, template_file='../apidocs.yaml')
+
+
+@app.route('/parties')
+def colors(palette):
+    """
+    file:apidocs.yaml
+    """
+
+
+app.run(debug=True)
